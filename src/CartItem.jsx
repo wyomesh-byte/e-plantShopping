b727743b3e1d927c9b967ce7a6df9d33ddbe2dc6
wyomesh-byte <wyomesh.all.crse@gmail.com>
2024-09-18 02:43:40 -0400

    E-plant shopping website created

---
+++ src/CartItem.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeItem, updateQuantity } from './CartSlice';
+import './CartItem.css';
+
+const CartItem = ({ onContinueShopping }) => {
+    const cart = useSelector(state => state.cart.items);
+    const dispatch = useDispatch();
+
+    // Calculate total amount for all products in the cart
+    const calculateTotalAmount = () => {
+        let sum = 0;
+        cart.forEach(element => {
+            const quantity = element.quantity;
+            const cost = Number(element.cost.slice(1))
+            sum += quantity * cost;
+        });
+        return sum;
+    };
+
+    const handleContinueShopping = (e) => {
+        onContinueShopping(e);
+    };
+
+    const handleCheckoutShopping = (e) => {
+        e.preventDefault();
+        alert("Functionality to be added for future reference")
+    }
+
+    const handleIncrement = (item) => {
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
+    };
+
+    const handleDecrement = (item) => {
+        if (item.quantity > 1) {
+            dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+        } else {
+            dispatch(removeItem(item.name));
+        }
+    };
+
+    const handleRemove = (item) => {
+        dispatch(removeItem(item.name));
+    };
+
+    // Calculate total cost based on quantity for an item
+    const calculateTotalCost = (item) => {
+        const quantity = item.quantity;
+        const cost = Number(item.cost.slice(1))
+        return quantity * cost;
+    };
+
+    return (
+        <div className="cart-container">
+            <h2 style={{ color: 'black' }}>Total Cart Amount: ${calculateTotalAmount()}</h2>
+            <div>
+                {cart.map(item => (
+                    <div className="cart-item" key={item.name}>
+                        <img className="cart-item-image" src={item.image} alt={item.name} />
+                        <div className="cart-item-details">
+                            <div className="cart-item-name">{item.name}</div>
+                            <div className="cart-item-cost">{item.cost}</div>
+                            <div className="cart-item-quantity">
+                                <button className="cart-item-button cart-item-button-dec" onClick={() => handleDecrement(item)}>-</button>
+                                <span className="cart-item-quantity-value">{item.quantity}</span>
+                                <button className="cart-item-button cart-item-button-inc" onClick={() => handleIncrement(item)}>+</button>
+                            </div>
+                            <div className="cart-item-total">Total: ${calculateTotalCost(item)}</div>
+                            <button className="cart-item-delete" onClick={() => handleRemove(item)}>Delete</button>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div style={{ marginTop: '20px', color: 'black' }} className='total_cart_amount'></div>
+            <div className="continue_shopping_btn">
+                <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
+                <br />
+                <button className="get-started-button1" onClick={handleCheckoutShopping}>Checkout</button>
+            </div>
+        </div>
+    );
+};
+
+export default CartItem;
+
+
